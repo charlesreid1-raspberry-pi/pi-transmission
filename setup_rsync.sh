f88522590742f8bd08249c89783ae1b67c115ca7
Charles Reid <charlesreid1@gmail.com>
2017-03-31 23:10:31 -0700

    re-writing setup_rsync

--- setup_rsync.sh
+++ setup_rsync.sh
@@ -3,15 +3,9 @@
 # run this on the raspberry pi 
 # to set up rsync from the client side.
 
-# set path for rsyncing
-CLIENT_NAME="${HOSTNAME}"
-CLIENT_PATH="${HOME}/wifi/"
-REMOTE_PATH="/wifi/" # reidmachine.party
+echo "Backing up /etc/rsyncd.conf to /etc/rsyncd.conf.bak"
+mv /etc/rsyncd.conf{,.bak}
 
-# fix up rsyncd.conf before putting it in /etc/rsyncd.conf
-sed "s'NAMENAME'${CLIENT_NAME}';s'PATHPATH'${CLIENT_PATH}'" rsync.conf > rsync.conf.new
-#cat rsync.conf.new
-cp rsync.conf.new /etc/rsync.conf
+echo "Installing rsyncd.conf to /etc/rsyncd.conf" 
+cp rsyncd.conf /etc/rsyncd.conf
 
-# clean up
-rm rsync.conf.new
