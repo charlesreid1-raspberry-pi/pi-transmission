862154b72b4ed5639801f62238ada5e22c47df91
Charles Reid <charlesreid1@gmail.com>
2017-04-01 06:17:28 +0000

    add existence check before backing up file

--- setup_rsync.sh
+++ setup_rsync.sh
@@ -3,8 +3,10 @@
 # run this on the raspberry pi 
 # to set up rsync from the client side.
 
-echo "Backing up /etc/rsyncd.conf to /etc/rsyncd.conf.bak"
-mv /etc/rsyncd.conf{,.bak}
+if [ -f /etc/rsyncd.conf ]; then
+	echo "Backing up /etc/rsyncd.conf to /etc/rsyncd.conf.bak"
+	mv /etc/rsyncd.conf{,.bak}
+fi
 
 echo "Installing rsyncd.conf to /etc/rsyncd.conf" 
 cp rsyncd.conf /etc/rsyncd.conf
