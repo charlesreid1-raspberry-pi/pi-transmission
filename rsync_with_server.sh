bcf8ab9395fc86b5b32d6cb6ab16c0191e40bba5
Charles Reid <charlesreid1@gmail.com>
2017-03-31 19:15:20 -0700

    adding a final rsync call

--- rsync_with_server.sh
+++ rsync_with_server.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+# 
+# This script synchronizes ${HOME}/wifi with <remote>::/wifi
+# over an encrypted stunnel connection.
+#
+# CSV files are timestamped with hostname/time/date and will be unique.
+#
+# The trick to using stunnel is, <remote>::/wifi is 127.0.0.1::/wifi 
+# over port X (remote connection is handled by stunnel).
+# 
+# see http://www.netbits.us/docs/stunnel_rsync.html
 
-# Behavior of program:
-# - aircrack batch creates csv files
-# - csv files are stamped with hostname/date/time
-# - rsync to upload/sync csv file directory w server
-# - wifi data in specified location on remote server
 
-client_wifi="${HOME}/wifi/"
+# destination on frohike is set in config file:
+# /wifi
+rsync -vv -aR /path/to/data.file localhost::frohike
 
